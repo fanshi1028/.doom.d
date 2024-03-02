f44783089065e8f5444a8c298545d352d7c9a364
fanshi1028 <jackychany321@gmail.com>
2024-03-02 09:50:42 +0800

    no pomodoro

--- init.el
+++ init.el
@@ -154,7 +154,7 @@
        ;;nim               ; python + lisp at the speed of c
        nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +pomodoro  +journal +dragdrop +noter +pandoc +gnuplot) ; organize your plain life in plain text
+       (org +journal +dragdrop +noter +pandoc +gnuplot) ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        plantuml                 ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
