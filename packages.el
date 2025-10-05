4138c3a8d5b4234ec8685dc0d2dff6046d42d02c
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-10-05 10:53:33 +0800

    no using org-ai

--- packages.el
+++ packages.el
@@ -88,9 +88,3 @@
 (package! synosaurus :pin "14d34fc92a77c3a916b4d58400424c44ae99cd81")
 
 (package! org-gtd :recipe (:host github :repo "Trevoke/org-gtd.el") :pin "578e83b0f67cb57dd1b10e9eea4f40d2e925b9b9")
-
-;; (package! aider :recipe (:host github :repo "tninja/aider.el" :files ("aider.el" "aider-doom.el")) :pin "b412d331e54ec13c66ef221e7865d29db91ec41c")
-;; 
-;; (package! org-ai
-;;   :recipe (:host github :repo "rksm/org-ai")
-;;   :pin "5a906fd4ecc4ff4d8ad561da14346a9d8b1d17db")
