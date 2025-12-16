462a51f0e154bc7e7b766c98ae898b83d0e2e82c
fanshi1028 <8843502+fanshi1028@users.noreply.github.com>
2025-12-16 21:07:14 +0800

    not using org-noter

--- init.el
+++ init.el
@@ -157,7 +157,7 @@
        ;;nim               ; python + lisp at the speed of c
        nix                              ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +journal +dragdrop +noter +pandoc +gnuplot) ; organize your plain life in plain text
+       (org +journal +dragdrop +pandoc +gnuplot) ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
